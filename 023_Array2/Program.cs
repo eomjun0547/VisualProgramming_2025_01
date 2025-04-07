940ca4a63242551749a8dcade6cb04eb303a41bd
eomjun0547 <eomjun0407@gmail.com>
2025-04-07 14:46:13 +0900

    Add files via upload

---
+++ 023_Array2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _023_Array2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] a = new int[20];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = r.Next(0, 100);
+            }
+
+            Console.WriteLine("배열 값: ");
+            foreach (var i in a)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine(); 
+
+            int min = a[0];
+            int max = a[0];
+            int sum = 0;
+
+            foreach (var i in a)
+            {
+                if (i < min)
+                    min = i;
+                if (i > max)
+                    max = i;
+                sum += i;
+            }
+
+            Console.WriteLine("min = {0}, max = {1}, avg = {2:F2}", min, max, sum / 20.0);
+        }
+    }
+}
---
+++ 024_Class/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _024_Clasee
+{
+    public class Program
+    {
+        class Date
+        {
+            private int year, month, day;
+            public void SetYear(int year)
+            {
+                this.year = year;
+            }
+            public int GetYear()
+            {
+                return this.year;
+            }
+            public void PrintDate()
+            {
+                Console.WriteLine("{0}년 {1}월 {2}일", year, month, day);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Date a = new Date();
+            a.SetYear(2025);
+            Console.WriteLine(a.GetYear());
+            a.PrintDate();
+        }
+    }
+}
